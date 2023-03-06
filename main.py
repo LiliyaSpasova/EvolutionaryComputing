66cf1c15889fff8907aafad847ffe8dffca8a14f
Liliya Spasova <liliya.nspasova@gmail.com>
2023-03-06 22:17:24 +0200

    add schemata barplot

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ import random
 import fitnesses
 import time
 import matplotlib.pyplot as plt
+import pandas as pd
 
 length=40
-populationSize=180
-isUniformCrossover=False
+populationSize=200
+isUniformCrossover=True
 isTighlyLinked=True
-fitness=2
+fitness=0
 dataForOnesPlot=[]
+Iterations=20
+
+dataForSchemataPlot=[]
 hexadecimal_alphabets = '0123456789ABCDEF'
 fitnessCalculations=[]
 iterationsCount=[]
 colors = ["#" + ''.join([random.choice(hexadecimal_alphabets) for _ in range(6)]) for _ in range(20)]
+optimalSolution=""
+for i in range(0,length):
+    optimalSolution+="1"
 
 # we say that counting ones i 0, trap func is 1 and, deceptive trap is 2
 def countOnesInPopulation(population):
@@ -23,6 +30,14 @@ def countOnesInPopulation(population):
     for p in population:
         res+=fitnesses.calcFitnessOnes(p)
     return res
+
+def countMembersOfSchemata(population):
+    res=0
+    for p in population:
+        if(p[0]=='1'):
+            res+=1
+    return res
+
 def areChildrenGettingBetter(sortedCompetition):
     minFitnessOfParents = 500000
     maxFitnessOfChildren = -1
@@ -65,22 +80,18 @@ def pickAncestors(firstParent,secondParent,length,isUniformCrossover,fitness,isT
 
 def isSolutionFound(population):
 
-    found_res=False
-    optimalSolution=""
-
-    for i in range(0,length):
-        optimalSolution+="1"
-        
-    l= len(optimalSolution)
     for candidate in population:
         if candidate==optimalSolution:
-           found_res = True        
+           return True        
 
-    return found_res
+    return False
 
-def iterate(isUniformCrossover,fitness,isTighlyLinked):    
+def iterateWithPlots(isUniformCrossover,fitness,isTighlyLinked):    
     dataForOnesPlotX=[]
     dataForOnesPlotY=[]
+    schemataDataX=[]
+    schemataDataOne=[]
+    schemataDataZero=[]
     population=generateInitialPopulation.generateInitialPopulation(length=length,populationSize=populationSize)
     counter=0
     countIterations=0
@@ -96,6 +107,10 @@ def iterate(isUniformCrossover,fitness,isTighlyLinked):
             areBetter=ancestors[2]
             if areBetter :
                 areBetterForGeneration=True
+        schemataDataX.append(countIterations)
+        countMembersOfSchemataOne=countMembersOfSchemata(population)
+        schemataDataOne.append(countMembersOfSchemataOne)
+        schemataDataZero.append(populationSize-countMembersOfSchemataOne)
         dataForOnesPlotX.append(countIterations)
         dataForOnesPlotY.append(countOnesInPopulation(population)/(populationSize*length))
         population=newPopulation
@@ -105,28 +120,59 @@ def iterate(isUniformCrossover,fitness,isTighlyLinked):
         else:
             counter+=1
         countIterations+=1
-        
         if isSolutionFound(population):
             dataForOnesPlot.append((dataForOnesPlotX,dataForOnesPlotY))
+            dataForSchemataPlot.append((schemataDataX,schemataDataZero,schemataDataOne))
             dataForOnesPlotX=[]
             dataForOnesPlotY=[]
+            schemataDataX=[]
+            schemataDataOne=[]
+            schemataDataZero=[]
             return (True,countIterations)
         if counter==10:
             dataForOnesPlot.append((dataForOnesPlotX,dataForOnesPlotY))
+            dataForSchemataPlot.append((schemataDataX,schemataDataZero,schemataDataOne))
             dataForOnesPlotX=[]
-            dataForOnesPlotY=[]
+            dataForOnesPlotY=[] 
+            schemataDataX=[]
+            schemataDataOne=[]
+            schemataDataZero=[]
+            return (False,countIterations)
+        
+def iterateWithoutPlots(isUniformCrossover,fitness,isTighlyLinked):    
+    population=generateInitialPopulation.generateInitialPopulation(length=length,populationSize=populationSize)
+    counter=0
+    countIterations=0
+    while True:      
+        random.shuffle(population)
+        newPopulation=[]
+        areBetterForGeneration=False
+        for i in range (0,populationSize-1,2):
+            areBetter=False
+            ancestors=pickAncestors(population[i],population[i+1],length,isUniformCrossover,fitness,isTighlyLinked)
+            newPopulation.append(ancestors[0])
+            newPopulation.append(ancestors[1])
+            areBetter=ancestors[2]
+            if areBetter :
+                areBetterForGeneration=True
+        population=newPopulation
+        if areBetterForGeneration:
+            counter=0 
+        else:
+            counter+=1
+        countIterations+=1
+        if isSolutionFound(population):
+            return (True,countIterations)
+        if counter==10:
             return (False,countIterations)
         
 
 def main ():
-  #  print("crossover",isUniformCrossover?"uniform crossover":"two point crossover")
-   # print("average iterations=",allIterations/20)
-    #print ("iterations count standard deviation=",numpy.std(iterationsCount))
     successes=0
     allIterations=0
     fitnessEvals=0
-    for i in range(0,20):
-        res=iterate(isUniformCrossover,fitness,isTighlyLinked)
+    for i in range(0,Iterations):
+        res=iterateWithoutPlots(isUniformCrossover,fitness,isTighlyLinked)
         if res[0]:
             successes+=1
         allIterations+=res[1]
@@ -142,8 +188,16 @@ def main ():
     print ("fitness evals count standard deviation=",numpy.std(fitnessCalculations))
     print("total time",totalTime)
     i=0
+   
+    fig, (ax1, ax2,ax3) = plt.subplots(1, 3)
+    fig.suptitle('Plots')
+    for (iteartions,ones,zeros) in dataForSchemataPlot:
+        ax1.bar(iteartions, ones, width=0.3, color='red', align='edge')
+        ax1.bar(iteartions, zeros, width=0.3, color='blue', align='center')
+        break
     for (x,y) in dataForOnesPlot:
-        plt.plot(x,y,color=colors[i])
+        ax2.plot(x,y,color=colors[i])
+        ax3.plot(x,y,color=colors[i])
         i+=1
     plt.show()
     return successes>=19
